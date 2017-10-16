06dc0dd27796f2ca1209c5460186ea59f2b1b1e7
scatt89 <dasilvagarciam@gmail.com>
2017-10-16 22:14:33 +0200

    Luhn test false case

--- spec/01_luhn_test_Spec.js
+++ spec/01_luhn_test_Spec.js
@@ -5,5 +5,6 @@ describe('01_luhn_test', () => {
 
     it('When redoce is invoked in people array the result should be', () => {
         expect(true).toEqual(luhnTest.test('49927398716'));
+        expect(false).toEqual(luhnTest.test('4992739871'));
     });
 });
